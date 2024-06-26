ce853fea7dcf9cb6b37e8adafccb9162a10ba220
Hugo Fortunier <h.fortunier@hookto.fr>
2024-06-26 11:01:18 +0200

    add countdown library

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-full block flex-grow flex items-center w-auto">
+        <div class="text-sm flex-grow">
+            <a href="#ou-et-quand" class="block mt-4 inline-block mt-0  mr-4">
+                Où et quand
+            </a>
+            <a href="#la-journee" class="block mt-4 inline-block mt-0  mr-4">
+                La journée
+            </a>
+            <a href="#accueil" class="block mt-4 inline-block mt-0  mr-4">
+                A&C
+            </a>
+            <a href="#ou-dormir" class="block mt-4 inline-block mt-0  mr-4">
+                Où dormir
+            </a>
+            <a href="#reponse" class="block mt-4 inline-block mt-0  mr-4">
+                Réponse
+            </a>
+            <a href="#contacts" class="block mt-4 inline-block mt-0 ">
+                Contacts
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+
+/* Ajoutez ici tout style personnalisé si nécessaire */
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import Countdown from 'vue3-flip-countdown'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(Countdown)
 
 app.use(createPinia())
 app.use(router)
