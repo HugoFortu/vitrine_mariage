4ac948a1cd0faee7187f7900003e1f132cbf3929
Hugo Fortunier <h.fortunier@hookto.fr>
2024-07-02 09:19:15 +0200

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import 'tailwindcss/tailwind.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// @ts-ignore
 import App from './App.vue'
+// @ts-ignore
 import Countdown from 'vue3-flip-countdown'
 
 const app = createApp(App).use(Countdown)
