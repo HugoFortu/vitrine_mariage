af03be38650d08a4c8dec9928d981030f83183cd
Hugo Fortunier <h.fortunier@hookto.fr>
2024-07-03 12:03:31 +0200

    add new marker on map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -13,12 +13,13 @@
   const initialMap = ref(null);
   
   onMounted(()=> {
-      initialMap.value = L.map('map').setView([48.400002, 2.46667], 12);
+      initialMap.value = L.map('map').setView([48.4034789, 2.4668973], 12);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19, 
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(initialMap.value);
-      L.marker([48.400002, 2.46667]).addTo(initialMap.value);
+      L.marker([48.4034789, 2.4668973]).addTo(initialMap.value);
+      L.marker([48.139802, 2.803057]).addTo(initialMap.value);
   });
 
   </script>
